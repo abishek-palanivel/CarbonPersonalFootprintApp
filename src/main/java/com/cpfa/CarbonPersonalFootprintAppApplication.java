3ed0082ebbf6a3db55412d12b9e3c6d078b4c325
abishek <abishekpalanivel212@gmail.com>
2026-02-24 07:09:56 +0530

    Fix JSP loading: Configure resources to copy webapp folder to META-INF/resources

--- src/main/java/com/cpfa/CarbonPersonalFootprintAppApplication.java
+++ src/main/java/com/cpfa/CarbonPersonalFootprintAppApplication.java
@@ -2,10 +2,9 @@ package com.cpfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarbonPersonalFootprintAppApplication extends SpringBootServletInitializer {
+public class CarbonPersonalFootprintAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarbonPersonalFootprintAppApplication.class, args);
