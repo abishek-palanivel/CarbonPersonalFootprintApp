373601df052665c671e63e6cfd2bfcad46882cd2
abishek <abishekpalanivel212@gmail.com>
2026-02-25 06:21:03 +0530

    Final fix: Use WAR packaging with Spring Boot 3.2.0 for JSP support

--- src/main/java/com/cpfa/CarbonPersonalFootprintAppApplication.java
+++ src/main/java/com/cpfa/CarbonPersonalFootprintAppApplication.java
@@ -2,9 +2,10 @@ package com.cpfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarbonPersonalFootprintAppApplication {
+public class CarbonPersonalFootprintAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarbonPersonalFootprintAppApplication.class, args);
